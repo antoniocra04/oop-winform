04b35e924a7b45d0abe987f6aa9644288b83280b
Anton Ryazanov <antoniocra04@gmail.com>
2024-03-17 09:29:15 +0700

    feature(Models): добавил IdGenerator

---
+++ oop-winform/Models/IdGenerator.cs
@@ -0,0 +1,23 @@
+﻿namespace oop_winform.Model
+{
+    /// <summary>
+    /// Класс генератора id.
+    /// </summary>
+    public static class IdGenerator
+    {
+        /// <summary>
+        /// Статический счетчик.
+        /// </summary>
+        private static int _id;
+
+        /// <summary>
+        /// Генерирует id.
+        /// </summary>
+        /// <returns>id.</returns>
+        public static int GetId()
+        {
+            _id++;
+            return _id;
+        }
+    }
+}
