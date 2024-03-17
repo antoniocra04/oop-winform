e2935916a367e8a9f51809b6213577d1f0cfc396
Anton Ryazanov <antoniocra04@gmail.com>
2024-03-17 09:32:38 +0700

    feature(models): добавил покупателя, продукт и класс с константами

---
+++ oop-winform/Models/Constants.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_winform.Model
+{
+    /// <summary>
+    /// Содержит константы.
+    /// </summary>
+    static class Constants
+    {
+        /// <summary>
+        /// Цвет для полей с верными данными.
+        /// </summary>
+        public static Color okColor = Color.White;
+
+        /// <summary>
+        /// Цвет для полей с неверными данными.
+        /// </summary>
+        public static Color errorColor = Color.Red;
+    }
+}
