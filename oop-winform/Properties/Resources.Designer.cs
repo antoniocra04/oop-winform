667f28f66714a7b2b578753e3808b065057bd4ec
Anton Ryazanov <antoniocra04@gmail.com>
2024-03-26 18:35:27 +0700

    fix(sln): правки

--- oop-winform/Properties/Resources.Designer.cs
+++ oop-winform/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace ObjectOrientedPractics.Properties {
+namespace oop_winform.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace ObjectOrientedPractics.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ObjectOrientedPractics.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("oop_winform.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- oop-winform/Services/ValueValidator.cs
+++ oop-winform/Services/ValueValidator.cs
@@ -13,12 +13,12 @@ namespace oop_winform.Services
         /// <param name="value">Строка.</param>
         /// <param name="maxLength">Максимальная длина.</param>
         /// <param name="property">Имя свойства класса.</param>
-        static public void StringLengthCheck(string value, int maxLength, string property)
+        public static void StringLengthCheck(string value, int maxLength, string property)
         {
             if (value.Length > maxLength)
             {
                 throw new
-                    ArgumentException($"{property} должно быть меньше {maxLength} символов.");
+                    ArgumentException($"{property} expect to be less than {maxLength} symbols.");
             }
         }
 
